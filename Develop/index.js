1b7ca85781c5b34e63dc982e3f862f53e51391d4
joetrops5 <jtropss@aol.com>
2022-08-19 14:19:04 -0400

    Added and array of questions to my index.js file (still need to modify messages)

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,55 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What do you want your title to be?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Give a brief description of your Readme",
+    },
+    {
+      type: "input",
+      name: "Table of contents",
+      message: "Table of contents",
+    },
+    {
+      type: "input",
+      name: "Installation",
+      message: "Install your project",
+    },
+    {
+      type: "input",
+      name: "Usage",
+      message: "usage",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "license",
+    },
+    {
+      type: "input",
+      name: "Contributors",
+      message: "Who contributed to this Readme?",
+    },
+    {
+      type: "input",
+      name: "Tests",
+      message: "Tests",
+    },
+    {
+      type: "input",
+      name: "Questions",
+      message: "Questions",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
